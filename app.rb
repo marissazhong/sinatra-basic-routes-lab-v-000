e0bb36d008971740537011c0fab607d6f78ee523
Marissa Zhong <mwz2101@columbia.edu>
2018-07-12 13:23:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   get '/name' do
     resp = "My name is #{name}"
     puts resp
-    resp.status = 200
+    self.status = 200
   end
   get '/hometown' do
     "My hometown is #{hometown}"
