39985a317e8099c7238cc693d8b8daef7560e13c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-12 13:25:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is #{name}"
+    "My name is Marissa"
   end
   get '/hometown' do
-    "My hometown is #{hometown}"
+    "My hometown is Washington, DC"
   end
   get '/favorite-song' do
-    "My favorite song is #{favorite-song}"
+    "My favorite song is Danza Kuduro"
   end
 end
